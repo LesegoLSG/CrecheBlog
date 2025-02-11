18667dc63fb99661f35fb10a681afc062098c03b
LesegoLSG <lesegomhlongo78@gmail.com>
2025-02-11 00:47:08 -0600

    Create signout api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,13 @@
 export const test = ( req, res) =>{
     res.json({message:"API is working!!!"});
 }
+
+// Signout functionality
+export const signout = (req,res,next) =>{
+    try{
+        res.clearCookie('access_token').status(200).json('User has been signed out successfully');
+    }catch(error){
+        next(error);
+    }
+
+}

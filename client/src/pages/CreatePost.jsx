e945b9d965ece3e7a96fe12703ddf2532acc424e
LesegoLSG <lesegomhlongo78@gmail.com>
2025-02-27 20:30:21 -0600

    Modify heading

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -108,7 +108,7 @@ const CreatePost = () => {
   };
   return (
     <section className="max-w-4xl mx-auto p-4 min-h-screen">
-      <h1 className="text-lg font-bold text-center my-6">Create a post</h1>
+      <h1 className="heading text-center my-6">Create a post</h1>
       <form className="flex flex-col gap-4" onSubmit={handlePublishPost}>
         <div className="flex flex-col md:flex-row justify-center items-center gap-4">
           {/* Title input */}
